03244f5327d29d45c9520db22f0691c93ae8cfb8
hritikdangi3 <hritikdangi3@gmail.com>
2022-03-22 23:47:54 +0530

    initial

--- 1.js
+++ 1.js
@@ -2,4 +2,4 @@
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 18];
 console.log(arr.slice(1, 7)); //[ 2, 3, 4, 5, 6, 7 ]
 console.log(arr.splice(1, 7)); //[  2, 3, 4, 5,  6, 7, 8]
-console.log(arr.slice(1, 7));   //[9,18]
+console.log(arr.slice(1, 7))   //[9,18]
